19ea47fc79ba0bd0d8170bf99cd429b7d8b4d787
vitaliy.ts <vitaliy.ts@rockalab.com>
2024-11-10 16:21:27 +0200

    update Header component with new links and styles; localize header text and adjust button hover effects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import cn from 'classnames';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  const tCommon = useTranslations('common');
+  const t = useTranslations('header');
   const [isScrolled, setIsScrolled] = useState(false);
 
   const handleScroll = () => {
@@ -30,20 +30,32 @@ const Header = () => {
         [styles['sticky']]: isScrolled,
       })}
     >
-      <div className={cn('wrapper', styles['header__wrapper'])}>
-        <Link href="/" className={styles['header__logo']}>
-          <Image
-            className={styles['header__logo-img']}
-            src={`/images/logo.svg`}
-            width={60}
-            height={50}
-            quality={100}
-            alt="logo risemee"
-          />
-        </Link>
-        <Button href="/onboarding" outside={true}>
-          {tCommon('startQuiz')}
-        </Button>
+      <div className={styles['header__top']}>
+        <div className={styles['header__top-wrapper']}>
+          <div className={styles['header__top-links']}>
+            <Link href="/b2b" className={styles['header__top-link']}>
+              {t('b2b')}
+            </Link>
+            <Link href="/affiliate" className={styles['header__top-link']}>
+              {t('affiliateProgram')}
+            </Link>
+            <Link href="/shipping-payment" className={styles['header__top-link']}>
+              {t('shippingAndPayment')}
+            </Link>
+            <Link href="/bonus" className={styles['header__top-link']}>
+              {t('bonusProgram')}
+            </Link>
+            <Link href="/blog" className={styles['header__top-link']}>
+              {t('blog')}
+            </Link>
+            <Link href="/contact" className={styles['header__top-link']}>
+              {t('contactUs')}
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles['header__bottom']}>
+        <div className={styles['header__bottom-wrapper']}></div>
       </div>
     </header>
   );
