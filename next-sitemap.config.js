d2e3705093aa64bb9ec823c5bb913ab4c630c536
vitaliy.ts <vitaliy.ts@rockalab.com>
2024-11-28 00:41:30 +0200

    feat: expand sitemap exclusion to include localized 404 and 500 pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,7 +6,7 @@ module.exports = {
   priority: 0.7,
   sitemapSize: 10000,
   generateIndexSitemap: false,
-  exclude: ['/404', '/500'],
+  exclude: ['/404', '/500', '/cz/500', '/de/500', '/fr/500', '/cz/404', '/de/404', '/fr/404'],
   // robotsTxtOptions: {
   //   policies: [
   //     { userAgent: '*', allow: '/' },
