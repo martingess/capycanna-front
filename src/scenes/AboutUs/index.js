4564e2737aad4de2553a99db86ad654d66baed61
vitaliy.ts <vitaliy.ts@rockalab.com>
2024-12-03 17:29:53 +0200

    feat: enhance About Us page with new content and localization for multiple languages; update styles and structure

--- src/scenes/AboutUs/index.js
+++ src/scenes/AboutUs/index.js
@@ -1,8 +1,6 @@
 import { useTranslations } from 'next-intl';
 import Head from 'next/head';
 import Breadcrumbs from '@components/Breadcrumbs';
-import Link from 'next/link';
-import { IconPdf } from '@UI';
 import styles from './AboutUs.module.scss';
 import Features from '@components/Features';
 
@@ -15,10 +13,15 @@ const AboutUs = () => {
         <title>{t('metaTitle')}</title>
         <meta name="description" content={t('metaDescription')} />
       </Head>
-      <section className={styles['analysis']}>
-        <div className={styles['analysis__wrapper']}>
+      <section className={styles['about']}>
+        <div className={styles['about__wrapper']}>
           <Breadcrumbs />
-          <h1 className={styles['analysis__title']}>{t('title')}</h1>
+          <h1 className={styles['about__title']}>{t('title')}</h1>
+          <div
+            className={styles['about__text']}
+            dangerouslySetInnerHTML={{ __html: t.raw('text.top') }}
+          />
+          <Features translation={'aboutUs'} />
         </div>
       </section>
     </>
